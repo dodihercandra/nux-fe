c99c65860c45af9ebac8fc7c2b7649dec7504dbe
dodihercandra <dodi.hercandra@gmail.com>
2024-08-23 10:05:08 +0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+      <!-- navbar -->
+      <Navbar />
+  
+      <!-- content -->
+      <Nuxt />
+  
+      <!-- footer -->
+      <Footer />
+  
+    </div>
+  </template>
+  <script>
+    import Navbar from '@/components/web/navbar.vue'
+    import Footer from '@/components/web/footer.vue'
+    export default {
+      components: {
+        Navbar,
+        Footer
+      },
+    }
+  </script>
+  
+  <style>
+    body {
+      background-color: #ccdcec !important;
+    }
+  
+    .jumbotron {
+      background: #186399 !important;
+      color: white;
+    }
+  
+    .bg-navbar {
+      background: #12466B;
+      
+    }
+   
+   .bg-navbar a:hover{
+    
+    background-color: #1c669b;
+    border-radius: 5px;
+    width: 100%;
+   
+ }
+   
+   .dropdown-menu{
+    background-color: #0f456b;
+    border:none;
+  
+   }
+   .dropdown-item{
+    
+    color: white;
+   }
+
+    .btn-primary {
+      background-color: #1c669b;
+      border-color: #1c669b;
+    }
+  
+    .btn-primary:hover {
+      background-color: #1c669b;
+      border-color: #1c669b;
+    }
+  
+    .btn-primary-dark {
+      background-color: #0f456b;
+      border-color: #0f456b;
+      color: white;
+    }
+  
+    .btn-primary-dark:hover {
+      color: white;
+    }
+  
+    .carousel-item .img-fluid {
+      border-radius: .5rem;
+    }
+  
+    .card-category {
+      background-color: #dadada;
+    }
+  
+    .object-fit-cover {
+      object-fit: cover;
+    }
+  
+    .footer-top {
+      background-color: #186399;
+      color: white;
+    }
+  
+    .footer-bottom {
+      background-color: #12466B;
+    }
+    .col-6 {
+      color: #12466B;
+     
+    }
+    .col-12 {
+      color: #12466B;
+     
+    }
+   
+   .bg-hero[data-v-f12bf83a] {
+    background-color: black;
+    height: 500px;
+    
+    background-size: cover;
+    position: inherit ;
+   }
+   .text-justify[data-v-f12bf83a] {
+    color: white;
+    position: absolute;
+    margin-top: 100px;
+    margin-left: 20px;
+    
+   }
+  </style>
---
+++ pages/markets/index.vue
@@ -0,0 +1,136 @@
+<template>
+    <b-container class="mt- mb-5">
+      <div data-v-f12bf83a="" class="bg-hero" style="background-image: url(images/petikemas.png);">
+        <div data-v-f12bf83a="" class="col-5 text-justify" ><h1>FRP Markets In Indonesia</h1>
+        <h5>Kami telah berkecimpung di berbagai pasar di Indonesia. Dengan karakteristiknya, produk komposit fiberglass diminati di pasar-pasar berikut: Pabrik Kimia, Teknik Konstruksi, Pabrik Makanan dan Minuman, Industri Elektronik, dan Teknik Petrokimia.</h5></div> 
+        </div>
+      
+        <div class="container mt-5">
+          <div class="row">
+                <div class="col-3 text-center">
+                    <img src="/images/server.svg" alt="" class="img-fluid">
+                 <h3>Data Center</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/zoo.svg" alt="" class="img-fluid">
+                    <h3>Zoo</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/petrocemical.svg" alt="" class="img-fluid">
+                    <h3>Petrochemical</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/mining.svg" alt="" class="img-fluid">
+                    <h3>Mining</h3>
+                </div>
+            </div>
+        </div>
+        <div class="container py-5">
+          <div class="row">
+                <div class="col-3 text-center">
+                    <img src="/images/agrikulture.svg" alt="" class="img-fluid">
+                 <h3>Agriculture</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/plup&paper.svg" alt="" class="img-fluid">
+                    <h3>Pulp & Paper</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/fertilizer.svg" alt="" class="img-fluid">
+                    <h3>Fertilizer</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/watertreatmen.svg" alt="" class="img-fluid">
+                    <h3>Watertreatment</h3>
+                </div>
+          </div>
+        </div>
+        <div class="container py-5">
+          <div class="row">
+                <div class="col-3 text-center">
+                    <img src="/images/textile.svg" alt="" class="img-fluid">
+                 <h3>Textile</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/automotiv.svg" alt="" class="img-fluid">
+                    <h3>Automotive</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/marine.svg" alt="" class="img-fluid">
+                    <h3>Marine / Maritime</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/coolingtower.svg" alt="" class="img-fluid">
+                    <h3>Cooling Tower</h3>
+                </div>
+          </div>
+        </div>
+        <div class="container py-5">
+          <div class="row">
+                <div class="col-3 text-center">
+                    <img src="/images/factory.svg" alt="" class="img-fluid">
+                 <h3>Factory</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/powerplan.svg" alt="" class="img-fluid">
+                    <h3>Power Plan</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/apartemen.svg" alt="" class="img-fluid">
+                    <h3>Apartemen / Condo</h3>
+                </div>
+                <div class="col-3 text-center">
+                    <img src="/images/residancehousing.svg" alt="" class="img-fluid">
+                    <h3>Residence / Housing</h3>
+                </div>
+          </div>
+        </div>
+        <div class="container py-5">
+          <div class="row">
+                <div class="col-3 text-center">
+                    <img src="/images/eng&kontraktor.png" height="101" width="100" alt="" class="img-fluid">
+                 <h3>Engineering & Contractor</h3>
+                </div>
+          </div>
+        </div>
+        
+      
+    
+    </b-container>
+  </template>
+  
+  <script>
+    export default {
+  
+      //meta
+      head() {
+        return {
+          title: 'Posts - PT. Orindo Eratec | Integrated Solution For Your Needs',
+          meta: [{
+              hid: 'og:title',
+              name: 'og:title',
+              content: 'PT. Orindo Eratec | Integrated Solution For Your Needs'
+            },
+            {
+              hid: 'og:site_name',
+              name: 'og:site_name',
+              content: 'PT. Orindo Eratec | Integrated Solution For Your Needs'
+            },
+            {
+              hid: 'og:image',
+              name: 'og:image',
+              content: ''
+            },
+          ]
+        }
+      },
+  
+      
+  
+      
+    }
+  </script>
+  
+  <style>
+  
+  </style>
